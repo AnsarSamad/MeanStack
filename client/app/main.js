b1d1ca6f31e07cca29955dc1b00b6aaacfc5d07f
AnsarSamad <met2ansar@gmail.com>
2017-03-17 08:08:01 +0530

    Signed-off-by: AnsarSamad <met2ansar@gmail.com>

--- client/app/main.js
+++ client/app/main.js
@@ -1,5 +1,10 @@
 "use strict";
 const platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
 const app_module_1 = require("./app.module");
+const core_1 = require("@angular/core");
+// Enable production mode unless running locally
+if (!/localhost/.test(document.location.host)) {
+    core_1.enableProdMode();
+}
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
